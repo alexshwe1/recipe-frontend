af18ecff050c263ef1b2f76b42b016f086d28361
alexshwe1 <alexander.shwe@gmail.com>
2023-08-23 11:06:14 -0500

    form working, need to add security for http PUSH

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaPlus } from 'react-icons/fa';
+import RecipeForm from '../RecipeForm/RecipeForm';
 import './PageHeader.css';
-import { FaPlus } from 'react-icons/fa'; // Import the plus icon from react-icons/fa
 
 const PageHeader = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+  const handleSubmit = async (event, formData) => {
+    event.preventDefault();
+    handleClose();
+
+    try {
+      const response = await fetch('https://us-east-1.aws.data.mongodb-api.com/app/recipeapi-rnans/endpoint/recipe', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Handle successful response
+      } else {
+        // Handle error response
+      }
+    } catch (error) {
+      // Handle fetch error
+    }
+  };
+
   return (
     <div className="page-header">
       <h1 className='page-title'>RecipeCards</h1>
       <div className="add-icon-container">
-        <FaPlus className="add-icon" />
+        <FaPlus className="add-icon" onClick={handleShow} />
       </div>
+
+      <RecipeForm showModal={showModal} handleClose={handleClose} handleSubmit={handleSubmit} />
     </div>
   );
 }
---
+++ src/components/RecipeForm/RecipeForm.js
@@ -0,0 +1,106 @@
+// RecipeForm.js
+import React, { useState } from 'react';
+import { Modal, Form, Button } from 'react-bootstrap';
+import './RecipeForm.css';
+
+const RecipeForm = ({ showModal, handleClose, handleSubmit }) => {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('Dinner');
+  const [description, setDescription] = useState('');
+  const [ingredients, setIngredients] = useState([]);
+  const [instructions, setInstructions] = useState('');
+  const [minutesToCook, setMinutesToCook] = useState(0);
+
+  const handleIngredientChange = (index, value) => {
+    const updatedIngredients = [...ingredients];
+    updatedIngredients[index] = value;
+    setIngredients(updatedIngredients);
+  };
+
+  return (
+    <Modal show={showModal} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          <h1 className='form-modal-title'>
+            Create A Recipe
+          </h1>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={(e) => handleSubmit(e, { title, category, description, ingredients, instructions, minutesToCook })}>
+          <Form.Group controlId="recipeTitle">
+            <h5 className="form-title">Recipe Title</h5>
+            <Form.Control
+              type="text"
+              placeholder="Enter recipe title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="recipeCategory">
+            <h5 className="form-title">Category</h5>
+            <Form.Control
+              as="select"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="Dinner">Dinner</option>
+              <option value="Dessert">Dessert</option>
+              <option value="Snack">Snack</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="recipeDescription">
+            <h5 className="form-title">Description</h5>
+            <Form.Control
+              as="textarea"
+              placeholder="Enter recipe description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="recipeIngredients">
+            <h5 className="form-title">Ingredients</h5>
+            {ingredients.map((ingredient, index) => (
+              <Form.Control
+                key={index}
+                type="text"
+                placeholder={`Enter ingredient ${index + 1}`}
+                value={ingredient}
+                onChange={(e) => handleIngredientChange(index, e.target.value)}
+              />
+            ))}
+            <Button variant="outline-primary" onClick={() => setIngredients([...ingredients, ''])}>
+              Add Ingredient
+            </Button>
+          </Form.Group>
+          <Form.Group controlId="recipeInstructions">
+            <h5 className="form-title">Instructions</h5>
+            <Form.Control
+              as="textarea"
+              placeholder="Enter recipe instructions"
+              value={instructions}
+              onChange={(e) => setInstructions(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="recipeMinutesToCook">
+            <h5 className="form-title">Minutes to Cook</h5>
+            <Form.Control
+              type="number"
+              placeholder="Enter minutes to cook"
+              value={minutesToCook}
+              onChange={(e) => setMinutesToCook(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="recipeSubmit">
+            <Button variant="primary" type="submit" className="submit-button">
+              Create Recipe
+            </Button>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default RecipeForm;
+
